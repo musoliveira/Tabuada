7b352bd53a51c77a19ae591d8a3a98309fb7f580
musoliveira <105720178+musoliveira@users.noreply.github.com>
2022-06-01 12:00:51 -0300

    Add files via upload

---
+++ tabuada/js.js
@@ -0,0 +1,13 @@
+function tabuada(){
+    var num = parseInt (document.getElementById("num").value);
+    var resultado = document.getElementById('resposta');
+    var tabuada = '';
+
+    for(var cont =1; count <=10; count ++){
+       tabuada += num + "X" + count + "=" +
+       num*count+"<br/>";
+
+
+      resultado.innerHTML = tabuada;
+    }
+}
